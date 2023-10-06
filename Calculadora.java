c8460d09fa1249caa8d391458f09d5982c8a1666
MiguelCastillo125 <miguel.125x@gmail.com>
2023-10-05 18:28:05 -0600

    feat: :sparkles: Se añadió la funcionalidad de potencia
    Closes #6

--- Calculadora.java
+++ Calculadora.java
@@ -1,4 +1,4 @@
-public class Calcuadora{
+public class Calculadora{
     public static double calcularPotencia(double numeroBase,double numeroPotencia){
         return Math.pow(numeroBase, numeroPotencia);
     }
