e65a4d9250f14d8d7d24bc4d465e0e7e896fa537
Diego-Arreola <diegoarreolahernandez66@gmail.com>
2023-10-05 18:16:00 -0600

    feat: :sparkles: Añade multiplicacion
    Se creo la funcionalidad de multiplicacion de 2 numeros

---
+++ Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora{
+
+    public float Multiplicacion(float primerNumero, float segundoNumero){
+        float resultado = 0;
+        resultado = primerNumero * segundoNumero;
+        return resultado;
+    }
+
+}
