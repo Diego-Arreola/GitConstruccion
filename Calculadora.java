d0ef06a23edd06fff1fb6f669cf7de303d636810
EPolFlo20 <117046903+EPolFlo20@users.noreply.github.com>
2023-10-07 20:20:11 -0600

    Se añadio la función módulo

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,6 @@
 public class Calculadora{
-
+    public static double calcularModulo(double numero1, double numero2) {
+        double resultado = numero1 % numero2;
+        return resultado;
+    }
 }
