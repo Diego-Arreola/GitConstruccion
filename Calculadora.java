cfeff910173026d7d09479a2bfcdab8f8e0e7809
Diego-Arreola <diegoarreolahernandez66@gmail.com>
2023-10-09 14:39:56 -0500

    refactor: :art: Metodo static
    Se cambio el metodo calcularMultiplicacion para que fuera estatico

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,7 @@
 public class Calculadora{
-    public double calcularMultiplicacion(double numero1, double numero2){
+    public static double calcularMultiplicacion(double numero1, double numero2){
         double resultado = numero1 * numero2;
         return resultado;
-    }  
+        
+    }
 }
