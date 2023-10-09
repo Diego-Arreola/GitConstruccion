91c941e0262fc0265a3be62ffb6782db5908bb80
Diego-Arreola <diegoarreolahernandez66@gmail.com>
2023-10-08 23:56:46 -0600

    feat: :sparkles: Funcionalidad multiplicacion
    Se agrego la funcionalidad multiplicacion a la calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,6 @@
 public class Calculadora{
-
+    public double calcularMultiplicacion(double numero1, double numero2){
+        double resultado = numero1 * numero2;
+        return resultado;
+    }  
 }
